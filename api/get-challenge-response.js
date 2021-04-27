bf2bf29e21517503b1d8b8f2a8e4dd1eee051d7d
Orim Dominic <orimdominic@gmail.com>
2021-04-27 16:45:35 +0100

    segment functions

--- api/get-challenge-response.js
+++ api/get-challenge-response.js
@@ -1,8 +1,8 @@
-const {
-  createHmac,
-} = require('crypto');
+const { createHmac } = require("crypto");
 
-module.exports  = function getChallengeResponse(crcToken) {
-	const consumerSecret  = process.env.TWITTER_CONSUMER_SECRET;
-	return `sha256=${createHmac('sha256', consumerSecret).update(crcToken).digest('base64')}`
-}
+module.exports = function getChallengeResponse(crcToken) {
+  const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+  return `sha256=${createHmac("sha256", consumerSecret)
+    .update(crcToken)
+    .digest("base64")}`;
+};
--- api/listen.js
+++ api/listen.js
@@ -1,7 +1,18 @@
-import getChallengeResponse from "./get-challenge-response"
-module.exports = (req, res) => {
-	const {crc_token} = req.query
+import getChallengeResponse from "./get-challenge-response";
+
+module.exports = async (req, res) => {
+  const method = req.method.toLowerCase();
+  switch (method) {
+    case "get":
+      return sendChallengeResponse(req, res);
+    default:
+      return res.status(405).send("Method not implemented");
+  }
+};
+
+async function sendChallengeResponse(req, res) {
+  const { crc_token } = req.query;
   res.json({
     response_token: getChallengeResponse(crc_token),
-  })
+  });
 }
