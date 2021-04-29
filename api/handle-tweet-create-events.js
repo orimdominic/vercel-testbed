a94d66ce94a04f763510d5ccb936dd0493707f42
Orim Dominic <orimdominic@gmail.com>
2021-04-29 18:28:05 +0100

    log valid mentions

--- api/handle-tweet-create-events.js
+++ api/handle-tweet-create-events.js
@@ -1,4 +1,27 @@
 module.exports = async function handleTweetCreateEvents(payload, res) {
-  console.log(JSON.stringify(payload, null, 4));
+  const validMentions = payload.create_tweet_events
+    .filter((tweet) => {
+      // ignore retweets, but accept quotes
+      if (tweet.retweeted_status && !tweet.is_quote_status) {
+        return false;
+      }
+      // ignore tweets by self
+      if (tweet.user.screen_name === process.env.PICKATRANDOM_SCREEN_NAME) {
+        return false;
+      }
+      return true;
+    })
+    .map((tweet) => {
+      return {
+        created_at: tweet.created_at,
+        id_str: tweet.id_str,
+        in_reply_to_status_id: tweet.in_reply_to_status_id,
+        user_screen_name: tweet.user.screen_name,
+        user_id_str: tweet.user.id_str,
+        text: tweet.truncated ? tweet.extended_tweet.full_text : tweet.text,
+        entities: tweet.entities,
+      };
+    });
+  console.log(JSON.stringify(validMentions, null, 4));
   return;
 };
