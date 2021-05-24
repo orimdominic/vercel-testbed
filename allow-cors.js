421def1f7bc091153c14d4cfd8a9144852416772
Orim Dominic <orimdominic@gmail.com>
2021-05-24 15:41:24 +0100

    fix: remove credentials

--- allow-cors.js
+++ allow-cors.js
@@ -1,5 +1,5 @@
 export const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
+  // res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
